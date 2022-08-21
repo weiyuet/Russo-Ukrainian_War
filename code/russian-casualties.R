18070c3ac099eb10b51fd788a12fe8c6f632b6d8
weiyuet <weiyuet@gmail.com>
2022-08-22 02:18:00 +0800

    Updated 22 August

--- code/russian-casualties.R
+++ code/russian-casualties.R
@@ -14,7 +14,7 @@ russia_losses_personnel_long %>%
   drop_na() %>%
   ggplot(aes(x = date, y = value, colour = casualties)) +
   geom_line() +
-  geom_point(size = 0.65, show.legend = FALSE) +
+  geom_point(size = 0.6, show.legend = FALSE) +
   scale_y_log10(labels = label_number(big.mark = ",")) +
   theme_classic() +
   theme(legend.position = c(0.8, 0.6)) +
