98f5fd8f061957a32b422a62e1e53a2593aa78e6
weiyuet <weiyuet@gmail.com>
2022-09-10 13:40:12 +0800

    Updated 10 September

--- code/russian-casualties.R
+++ code/russian-casualties.R
@@ -1,30 +1,33 @@
 # Setup
 library(tidyverse)
 library(scales)
+library(glue)
 
 # Load data Russian casualties
 russia_losses_personnel <- read_csv("data/russia_losses_personnel.csv")
 
 # Wrangle data change data to long format
-russia_losses_personnel_long <-  russia_losses_personnel %>%
+russia_losses_personnel_long <- russia_losses_personnel %>%
   pivot_longer(cols = c(personnel, POW), names_to = "casualties", values_to = "value")
 
 # Plot Russian casualties
 russia_losses_personnel_long %>%
   drop_na() %>%
   ggplot(aes(x = date, y = value, colour = casualties)) +
-  geom_line(show.legend = FALSE) +
+  geom_line() +
   geom_point(aes(shape = casualties), size = 0.7) +
   scale_y_log10(labels = label_number(big.mark = ",")) +
   scale_x_date(date_breaks = "1 month", labels = label_date_short()) +
-  annotate(geom = 'text', x = as.Date('2022-09-04'), y = 60000, label = '49500', size = 3) + #peak
+  annotate(geom = "text", x = as.Date(glue("{max(russia_losses_personnel_long$date)}"), y = 63000, label = glue("{max(russia_losses_personnel_long$value)}"), size = 3) + # peak
   theme_classic() +
   theme(legend.position = c(0.8, 0.55)) +
   scale_colour_grey(start = 0.2, end = 0.45) +
-  labs(x = "", y = "",
-       colour = "", shape = "",
-       title = "Russian Casualties",
-       caption = "Source: Armed Forces of Ukraine, Ministry of Defense of Ukraine\nGraphic: @weiyuet")
+  labs(
+    x = "", y = "",
+    colour = "", shape = "",
+    title = "Russian Casualties",
+    caption = "Source: Armed Forces of Ukraine, Ministry of Defense of Ukraine\nGraphic: @weiyuet"
+  )
 
 # Save png
 ggsave("figures/russia-losses-personnel.png", width = 6, height = 4)
@@ -35,8 +38,10 @@ russia_losses_equipment <- read_csv("data/russia_losses_equipment.csv")
 # Wrangle data change data to long format
 russia_losses_equipment_long <- russia_losses_equipment %>%
   select(-"greatest losses direction") %>%
-  pivot_longer(cols = c(aircraft:`cruise missiles`),
-               names_to = "equipment", values_to = "value")
+  pivot_longer(
+    cols = c(aircraft:`cruise missiles`),
+    names_to = "equipment", values_to = "value"
+  )
 
 # Plot Russian equipment losses
 russia_losses_equipment_long %>%
@@ -46,9 +51,11 @@ russia_losses_equipment_long %>%
   scale_x_date(date_breaks = "2 month", labels = label_date_short()) +
   theme_classic() +
   theme(legend.position = "none") +
-  labs(x = "", y = "",
-       title = "Russian Equipment Lost",
-       caption = "Source: Armed Forces of Ukraine, Ministry of Defense of Ukraine\nGraphic: @weiyuet")
+  labs(
+    x = "", y = "",
+    title = "Russian Equipment Lost",
+    caption = "Source: Armed Forces of Ukraine, Ministry of Defense of Ukraine\nGraphic: @weiyuet"
+  )
 
 # Save png
 ggsave("figures/russia-losses-equipment.png", width = 8, height = 8)
