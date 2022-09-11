76f6731a9d120d7d2ed14882b4ee82df0be5bc31
weiyuet <weiyuet@gmail.com>
2022-09-11 13:40:40 +0800

    Updated September 11

--- code/russian-casualties.R
+++ code/russian-casualties.R
@@ -18,16 +18,16 @@ russia_losses_personnel_long %>%
   geom_point(aes(shape = casualties), size = 0.7) +
   scale_y_log10(labels = label_number(big.mark = ",")) +
   scale_x_date(date_breaks = "1 month", labels = label_date_short()) +
-  annotate(geom = "text", x = as.Date(glue("{max(russia_losses_personnel_long$date)}"), y = 63000, label = glue("{max(russia_losses_personnel_long$value)}"), size = 3) + # peak
+  annotate(geom = "text",
+           x = as.Date(glue("{max(russia_losses_personnel_long$date)}")),
+           y = max(russia_losses_personnel$personnel) + 15000, label = glue("{max(russia_losses_personnel$personnel)}"), size = 3) + # peak
   theme_classic() +
   theme(legend.position = c(0.8, 0.55)) +
   scale_colour_grey(start = 0.2, end = 0.45) +
-  labs(
-    x = "", y = "",
+  labs(x = "", y = "",
     colour = "", shape = "",
     title = "Russian Casualties",
-    caption = "Source: Armed Forces of Ukraine, Ministry of Defense of Ukraine\nGraphic: @weiyuet"
-  )
+    caption = "Source: Armed Forces of Ukraine, Ministry of Defense of Ukraine\nGraphic: @weiyuet")
 
 # Save png
 ggsave("figures/russia-losses-personnel.png", width = 6, height = 4)
@@ -38,10 +38,8 @@ russia_losses_equipment <- read_csv("data/russia_losses_equipment.csv")
 # Wrangle data change data to long format
 russia_losses_equipment_long <- russia_losses_equipment %>%
   select(-"greatest losses direction") %>%
-  pivot_longer(
-    cols = c(aircraft:`cruise missiles`),
-    names_to = "equipment", values_to = "value"
-  )
+  pivot_longer(cols = c(aircraft:`cruise missiles`),
+    names_to = "equipment", values_to = "value")
 
 # Plot Russian equipment losses
 russia_losses_equipment_long %>%
@@ -51,11 +49,9 @@ russia_losses_equipment_long %>%
   scale_x_date(date_breaks = "2 month", labels = label_date_short()) +
   theme_classic() +
   theme(legend.position = "none") +
-  labs(
-    x = "", y = "",
+  labs(x = "", y = "",
     title = "Russian Equipment Lost",
-    caption = "Source: Armed Forces of Ukraine, Ministry of Defense of Ukraine\nGraphic: @weiyuet"
-  )
+    caption = "Source: Armed Forces of Ukraine, Ministry of Defense of Ukraine\nGraphic: @weiyuet")
 
 # Save png
 ggsave("figures/russia-losses-equipment.png", width = 8, height = 8)
